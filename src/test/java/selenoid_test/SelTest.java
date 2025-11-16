3f8373a184c2cecf6c471ece4c5de5d33ac5f15e
Alex_299 <elrockstar25@gmail.com>
2025-11-16 18:12:42 +0300

    one positive test was added

--- src/test/java/selenoid_test/SelTest.java
+++ src/test/java/selenoid_test/SelTest.java
@@ -3,6 +3,7 @@ package selenoid_test;
 import base.SelBaseTest;
 import io.qameta.allure.*;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.AutorizationPage;
 
@@ -10,14 +11,17 @@ public class SelTest extends SelBaseTest {
 
     private static final String BASE_URL = "https://www.saucedemo.com/";
 
+    @BeforeMethod
+    public void beforeEach() {
+        getDriver().get(BASE_URL);
+    }
+
     @Epic(value = "E2E tests")
     @Feature(value = "Buying an item")
     @Severity(value = SeverityLevel.NORMAL)
     @Description("Buying an item. From an autorization to an order. NEGATIVE.")
     @Test(priority = 1, description = "Checking of creating an order. CHAINS")
     public void buyBackPack() {
-
-        getDriver().get(BASE_URL);
 
         String finishBuyHeader = new AutorizationPage(getDriver())
                 .inputLogin()
@@ -41,4 +45,32 @@ public class SelTest extends SelBaseTest {
         Assert.assertEquals(finishBuyHeader,"Thank you for order!");
     }
 
+    @Epic(value = "E2E tests")
+    @Feature(value = "Buying an item")
+    @Severity(value = SeverityLevel.NORMAL)
+    @Description("Buying an item. From an autorization to an order")
+    @Test(priority = 1, description = "Checking of creating an order. CHAINS")
+    public void buyBackPackTest() {
+
+        String finishBuyHeader = new AutorizationPage(getDriver())
+                .inputLogin()
+                .inputPassword()
+                .clickSubmit()
+                .clickOnBackPack()
+                .addBackPackToCart()
+                .clickBackPackCartIcon()
+                .clickCheckoutButton()
+                .fillThefirstNameField()
+                .fillThelastNameField()
+                .fillThepostCodeField()
+                .clickOnsumbitButton()
+                .checkoutHeaderStringCheck()
+                .checkPaymentInfo()
+                .checkShipInfo()
+                .checkPrice()
+                .finishButtonClick()
+                .getFinishBuyHeader();
+
+        Assert.assertEquals(finishBuyHeader,"Thank you for your order!");
+    }
 }
