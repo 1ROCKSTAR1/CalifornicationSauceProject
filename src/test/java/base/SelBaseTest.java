edb5eb439fe77a4dff03a2bb2d232f95882bb784
Alex_299 <elrockstar25@gmail.com>
2025-11-16 14:20:12 +0300

    selBaseTest was created. headless was turned off

---
+++ src/test/java/base/SelBaseTest.java
@@ -0,0 +1,64 @@
+package base;
+
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class SelBaseTest {
+
+    private WebDriver driver;
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        FirefoxOptions options = new FirefoxOptions();
+
+        options.setBrowserVersion("120.0");
+        //options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new RemoteWebDriver(
+                new URL("http://selenoid:4444/wd/hub"),
+                options
+        );
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(45));
+    }
+
+    @AfterMethod
+    protected void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            takeScreenshotOnFailure(result.getName());
+        }
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public void takeScreenshotOnFailure(String testName) {
+        try {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+            Allure.getLifecycle().addAttachment(testName, "image/png", ".png", screenshot);
+            System.out.println("✅ Screenshot taken for: " + testName);
+        } catch (Exception e) {
+            System.err.println("❌ Failed to take screenshot: " + e.getMessage());
+        }
+    }
+}
--- src/test/java/selenoid_test/SelTest.java
+++ src/test/java/selenoid_test/SelTest.java
@@ -1,47 +1,12 @@
 package selenoid_test;
 
+import base.SelBaseTest;
 import io.qameta.allure.*;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page.AutorizationPage;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SelTest {
-
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        FirefoxOptions options = new FirefoxOptions();
-
-        options.setBrowserVersion("120.0");
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-
-        driver = new RemoteWebDriver(
-                new URL("http://selenoid:4444/wd/hub"),
-                options
-        );
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(45));
-    }
+public class SelTest extends SelBaseTest {
 
     @Epic(value = "E2E tests")
     @Feature(value = "Buying an item")
@@ -50,7 +15,7 @@ public class SelTest {
     @Test(priority = 1, description = "Checking of creating an order. CHAINS")
     public void buyBackPack() {
 
-        String finishBuyHeader = new AutorizationPage(driver)
+        String finishBuyHeader = new AutorizationPage(getDriver())
                 .inputLogin()
                 .inputPassword()
                 .clickSubmit()
@@ -72,24 +37,4 @@ public class SelTest {
         Assert.assertEquals(finishBuyHeader,"Thank you for order!");
     }
 
-    @AfterMethod
-    protected void tearDown(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            takeScreenshotOnFailure(result.getName());
-        }
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    public void takeScreenshotOnFailure(String testName) {
-        try {
-            TakesScreenshot ts = (TakesScreenshot) driver;
-            byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-            Allure.getLifecycle().addAttachment(testName, "image/png", ".png", screenshot);
-            System.out.println("✅ Screenshot taken for: " + testName);
-        } catch (Exception e) {
-            System.err.println("❌ Failed to take screenshot: " + e.getMessage());
-        }
-    }
 }
